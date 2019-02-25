6c0825b333989c5141a2b86020fcedb8ed4988bf
maruthi0611 <maruthi464@gmail.com>
2019-02-25 11:00:30 +0530

    dropped missing value columns

--- mpneuron.py
+++ mpneuron.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[2]:
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -38,7 +43,11 @@ testdf = pd.read_csv("C:\\Users\\mkanukolanu\\Downloads\\padhai-module1-assignme
 traininp['class'] = traininp['Rating'].map(lambda x:1 if x>=4 else 0)
 
 X_raw = traininp.drop(['PhoneId','Rating','class'],axis=1)
-X = X_raw.notnull().astype('int')
+drop_columns = ['Also Known As', 'Applications', 'Audio Features', 'Bezel-less display',                 'Browser', 'Build Material', 'Co-Processor', 'Custom UI', 'Display Colour',                'Email', 'Games', 'HDMI', 'Heart Rate Monitor', 'IRIS Scanner', 'Java',                'Mobile High-Definition Link(MHL)', 'Music', 'Other Facilities', 'Phone Book',                'Ring Tone', 'Ruggedness', 'SAR Value', 'SIM 3', 'SMS',                 'Screen to Body Ratio (claimed by the brand)', 'Sensor', 'Software Based Aperture',                'Special Features', 'Standby time', 'Stylus', 'TalkTime', 'USB Type-C', 'User Available Storage',                'Video Player', 'Video Recording Features', 'Waterproof', 'Wireless Charging','Screen Protection',                'Optical Image Stabilisation','Aspect Ratio','Fingerprint Sensor Position','USB OTG Support']
+
+X_clean = X_raw.notnull().astype('int')
+
+X = X_clean.drop(drop_columns,axis=1)
 
 Y = traininp[['class']]
 
@@ -58,7 +67,8 @@ accuracy_test = accuracy_score(Y_test_pred, Y_test)
 print(accuracy_test)
 
 X1_raw = testdf.drop(['PhoneId'],axis=1)
-X1 = X1_raw.notnull().astype('int')
+X1_clean = X1_raw.notnull().astype('int')
+X1 = X1_clean.drop(drop_columns,axis=1)
 X1 = X1.values
 
 Y1_pred = mp_neuron.predict(X1)
@@ -66,4 +76,5 @@ Y1_pred = mp_neuron.predict(X1)
 finaldf = pd.concat([testdf[['PhoneId']], pd.DataFrame(Y1_pred.astype('int'))], axis=1)
 finaldf.columns = ['PhoneId','Class']
 
-finaldf.to_csv("C:\\Users\\mkanukolanu\\Downloads\\padhai-module1-assignment\\mysubmission1.csv",index=False)
+finaldf.to_csv("C:\\Users\\mkanukolanu\\Downloads\\padhai-module1-assignment\\mysubmission3.csv",index=False)
+
